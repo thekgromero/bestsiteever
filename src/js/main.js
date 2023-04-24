19e533639d8b95eae9873d6412064bf2eb1eaf91
thekgromero <kgromero.i3c.technologies@gmail.com>
2023-04-24 19:02:53 +0200

    🎉 First commit

---
+++ src/js/main.js
@@ -0,0 +1,9 @@
+// Import our custom CSS
+import '../scss/styles.scss'
+
+// Import all of Bootstrap's JS
+import * as bootstrap from 'bootstrap'
+
+var warning = new bootstrap.Modal(document.getElementById('warning'), {})
+
+//warning.show()
